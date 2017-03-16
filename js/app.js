00ee2816f5c2c7641b79942f235c1fdc71841043
yang lizhi <271071524@qq.com>
2017-03-16 10:50:16 +0800

    添加任务功能完成

--- js/app.js
+++ js/app.js
@@ -15,5 +15,31 @@
 				{id:2, name:'喝酒',isCompleted:true},
 				{id:3, name:'烫头发',isCompleted:false}
 			];
-		}])
+            //1.展示任务列表
+            //通过ng-repeat指令生成的结构
+
+            //2.添加任务
+            $scope.newTask='';
+            $scope.add=function(){
+                //1.获取到用户输入的内容
+                //2.拼接出一个对象
+                //3.放到任务数组中
+                if($scope.newTask.trim()===''){
+                    return;
+                }
+
+                //
+                var id;
+                if($scope.taskList.length===0){
+                    id=1;
+                }else{
+                    var index=$scope.taskList.length-1;
+                    id=$scope.taskList[index].id+1;
+                }
+                $scope.taskList.push({id:id,name:$scope.newTask,isCompleted:false});
+
+                //重置任务名称
+                $scope.newTask='';
+            }
+		}]);
 })(angular);
