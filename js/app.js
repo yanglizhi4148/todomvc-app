faf6ba1b373b653d096301e4b672dc2b03043411
yang lizhi <271071524@qq.com>
2017-03-16 10:36:45 +0800

    展示任务列表完成

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-(function (window) {
+(function (angular) {
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
-
-})(window);
+	angular
+		.module('todo',[])
+		.controller('TodoController',['$scope',function($scope){
+			//数据结构
+            //task任务
+			$scope.taskList=[
+                //id用于区分每一个任务
+                //name表示任务名称
+                //isCompleted表示任务是否完成
+				{id:1, name:'抽烟',isCompleted:false},
+				{id:2, name:'喝酒',isCompleted:true},
+				{id:3, name:'烫头发',isCompleted:false}
+			];
+		}])
+})(angular);
